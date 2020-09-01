281460382d262b160c67ad74bb64f7ea7c261a69
junstyle <junstyle@git>
2020-09-01 11:45:55 +0800

    update

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,96 +1,96 @@
-var fs       = require('fs'),
+var fs = require('fs'),
     testPath = __dirname + '/tests/',
-    tests    = fs.readdirSync(testPath),
-    server   = require('./server'),
-    cycles   = 500;
+    tests = fs.readdirSync(testPath),
+    server = require('./server'),
+    cycles = 1
 
 function Timer(callback) {
-    this.events = [];
-    this.callback = callback;
+    this.events = []
+    this.callback = callback
 
-    return this;
+    return this
 }
 
 Timer.prototype.start = function () {
     this.events.push({
         name: 'start',
         time: ms(),
-        duration: 0
-    });
-};
+        duration: 0,
+    })
+}
 
 Timer.prototype.done = function (name) {
     var event = {},
         length = this.events.length,
-        prev = this.events[length - 1];
+        prev = this.events[length - 1]
 
-    event.name     = name;
-    event.time     = ms();
-    event.duration = event.time - prev.time;
+    event.name = name
+    event.time = ms()
+    event.duration = event.time - prev.time
 
-    this.events.push(event);
-};
+    this.events.push(event)
+}
 
 Timer.prototype.stop = function () {
     var event = {
-            name: 'stop',
-            time: ms()
-        },
-        stop = this.events[0];
+        name: 'stop',
+        time: ms(),
+    },
+        stop = this.events[0]
 
-    event.duration = event.time - stop.time;
+    event.duration = event.time - stop.time
 
-    this.events.push(event);
-    this.callback.call(this);
-};
+    this.events.push(event)
+    this.callback.call(this)
+}
 
 function ms() {
-    return (new Date()).getTime();
+    return (new Date()).getTime()
 }
 
 (function loadTest(i) {
-    var test = require(testPath + tests[i]);
+    var test = require(testPath + tests[i])
 
     loadBenchmark(test, Object.keys(test), 0, function () {
         if (++i < tests.length) {
-            loadTest(i);
+            loadTest(i)
         } else {
-            testsFinished();
+            testsFinished()
         }
-    });
-})(0);
+    })
+})(0)
 
 function loadBenchmark(benchmark, keys, index, done) {
-    var name = keys[index];
+    var name = keys[index]
 
-    console.log("Starting " + name + " - " + cycles + " cycles");
+    console.log("Starting " + name + " - " + cycles + " cycles")
     runBenchmark(name, benchmark[name], function () {
         if (keys.length < ++index) {
-            loadBenchmark(benchmark, keys, index, done);
+            loadBenchmark(benchmark, keys, index, done)
         } else {
-            done();
+            done()
         }
-    }, []);
+    }, [])
 }
 
 function runBenchmark(name, start, done, array) {
     start(new Timer(function () {
-        var total = 0;
+        var total = 0
 
-        array.push(this.events[this.events.length - 1].duration);
+        array.push(this.events[this.events.length - 1].duration)
 
         if (array.length === cycles) {
             array.forEach(function (duration) {
-                total += duration;
-            });
-            console.log("Timing (" + name + "): " + (total / array.length) + 'ms');
-            done();
+                total += duration
+            })
+            console.log("Timing (" + name + "): " + (total / array.length) + 'ms')
+            done()
         } else {
-            runBenchmark(name, start, done, array);
+            runBenchmark(name, start, done, array)
         }
-    }), server.url);
+    }), server.url)
 }
 
 function testsFinished() {
-    server.close();
+    server.close()
 }
